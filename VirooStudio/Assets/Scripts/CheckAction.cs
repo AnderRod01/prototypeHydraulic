2b0bf07a79291d7a03d6ef317f5350c87c9d07a2
AnderRod01 <80098555+AnderRod01@users.noreply.github.com>
2024-03-13 18:21:31 +0100

    check button added

---
+++ VirooStudio/Assets/Scripts/CheckAction.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VirooLab.Actions
+{
+    public class CheckController : BroadcastObjectAction
+    {
+
+        public void CheckSolution()
+        {
+
+        }
+    }
+}
--- VirooStudio/Assets/Scripts/GameController.cs
+++ VirooStudio/Assets/Scripts/GameController.cs
@@ -6,11 +6,7 @@ public class GameController : MonoBehaviour
 {
     public string selectedComponent = "";
 
+
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
     
 }
